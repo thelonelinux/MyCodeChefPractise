960646483a35e4f92bd9010b6db0ca091a1c004c
Vicky <vickythetechguy@gmail.com>
2019-11-11 19:41:02 +0530

    Until Todays Stuff

---
+++ ChaloAndroidHiring/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        List<Integer> list=new ArrayList<>();
+
+        for (int i=0;i<n;i++){
+
+            int num=sc.nextInt();
+
+
+            if (num==1){
+
+                //enter the value to be entered
+
+                int value=sc.nextInt();
+
+                int size=list.size();
+
+                if (size==0){
+                    list.add(value);
+
+                }
+
+                if (size==1){
+                    list.add(value);
+
+                }
+
+                if (size%2==0  && size!=0){
+                    
+                }
+            }
+        }
+    }
+}
--- ForPractise/src/com/company/Main.java
+++ ForPractise/src/com/company/Main.java
@@ -1,12 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+   static void printRepeating(int arr[], int size)
+    {
+        int i;
+        System.out.println("The repeating elements are : ");
 
-        int n=2;
+        for (i = 0; i < size; i++)
+        {
+            if (arr[ Math.abs(arr[i])] >= 0)
+                arr[ Math.abs(arr[i])] = -arr[ Math.abs(arr[i])];
+            else
+                System.out.print(Math.abs(arr[i]) + " ");
+        }
+    }
 
-        String str= String.valueOf(2);
+    // Driver program
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 2, 3, 1, 6, 6, 6};
+        int arr_size = arr.length;
+
+        printRepeating(arr, arr_size);
     }
 }
